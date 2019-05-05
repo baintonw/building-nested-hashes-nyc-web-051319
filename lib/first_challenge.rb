844bc88ffd7be7732484b6ec42bbeebddb105620
William Bainton <baintonw@gmail.com>
2019-05-05 16:52:10 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,10 +1,10 @@
 def first_challenge
    epic_tragedy = {
      :montague => {
-       :patriarch =>
-       :matrirch =>
-       :helper_method =>
-       :hero_friends =>
+       :patriarch => {}
+       :matrirch => {}
+       :helper_method => {}
+       :hero_friends => []
      }
      :capulet => {}
    }
